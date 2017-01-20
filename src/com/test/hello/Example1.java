1877dce0cdd673746cfdbbff5bb4b6f2e589ec52
Test <tummala.swathi@gmail.com>
2017-01-20 03:10:42 +0000

    My Second Commit

---
+++ src/com/test/hello/Example1.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class Example1 {
+    
+      public static void main(String[] args) {
+	    // write your code here
+	    System.out.println("Hello World 222******************");
+    }
+}
