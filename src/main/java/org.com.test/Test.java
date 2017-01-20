1ba2f0a76fc3134556bd3f6ee6ebcd0021ca46c2
Test <tummala.swathi@gmail.com>
2017-01-20 04:05:57 +0000

    commit

---
+++ src/main/java/org.com.test/Test.java
@@ -0,0 +1,9 @@
+package org.com.test;
+
+public class Test {
+    
+    public static void main(String[] argvs) {
+        String a = "Testtttttt*******";
+        System.out.println("Newwwww  Hello World " + a + "!");
+    }
+}
